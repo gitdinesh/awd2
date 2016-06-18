1ddf1e9c51a96ddcc98255ea6c8bd6c59bc4e7ab
piusha <piushakalyana@gmail.com>
2016-06-18 04:30:37 +0000

    Add First shel script file to the git

---
+++ first.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+clear
+echo "List all files in this directory "
+ls -al
+
+
+
+echo " -========================================="
+echo "Hey $USER"
+echo "Today is ==";date
+echo "Number of users login";who | wc -l
+
+
+echo "=============== Calender================="
+cal
+
+
+
+
